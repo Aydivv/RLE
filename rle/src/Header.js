4defc356e026936d32b07450f0417de841118b7d
Aydiv <55044178+Aydivv@users.noreply.github.com>
2022-02-25 17:39:43 +0000

    Initial commit
    created react app

---
+++ rle/src/Header.js
@@ -0,0 +1,14 @@
+import React from 'react'
+import './css/Header.css'
+
+function Header() {
+  return (
+    <div className='header'>
+        <div className="header__container">
+            <h1 className="header__title"> Run Length Encoder </h1>
+        </div>
+    </div>
+  )
+}
+
+export default Header
