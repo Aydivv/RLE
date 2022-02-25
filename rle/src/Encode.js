229ecb749a398b1198aff4924ac1de5abd60efe9
Aydiv <55044178+Aydivv@users.noreply.github.com>
2022-02-25 22:08:11 +0000

    Encoded functionality and css position completed

--- rle/src/Encode.js
+++ rle/src/Encode.js
@@ -1,28 +1,50 @@
 import React, { useState } from "react";
 import "./css/Encode.css";
 import { encoder } from "./logic";
+import TextareaAutosize from 'react-textarea-autosize';
 
 function Encode() {
-    const [string, setString] = useState("");
-    const [output, setOutput] = useState("");
-    const [caseToggle, setCaseToggle] = useState(true)
+  const [string, setString] = useState("");
+  const [output, setOutput] = useState("");
+  const [caseToggle, setCaseToggle] = useState(true);
 
-    const encode = e => {
-        e.preventDefault()
+  const encode = (e) => {
+    e.preventDefault();
 
-        let input = string.replace(/\s+/g,' ')
-        setOutput(encoder(input,caseToggle))
-    }
+    console.log(string)
+    let input = string.replace(/\s+/g, " ");
+    setOutput(encoder(input, caseToggle));
+  };
   return (
     <div className="encode__container">
-      <p>Enter a string to encode</p>
-      <form>
-      <input className="encode__input" value ={string} onChange={e => setString(e.target.value)} type="text"></input>
-      <button className="encode__submit" onClick={encode} type="submit"> Encode </button>
-      </form>
-      <p className="encode__output">{output || ""}</p>
+      <div className="encode__items">
+        <div className="encode__item">
+          <p className="encode__inputText">Enter a string to encode</p>
+        </div>
+        <div className="encode__item">
+          <form className="encode__form">
+            <TextareaAutosize
+              className="encode__input"
+              value={string}
+              onChange={(e) => {setString(e.target.value)}}
+            />
+            <button
+              className="encode__submit"
+              onClick={encode}
+              type="submit"
+            >
+              {" "}
+              Encode{" "}
+            </button>
+          </form>
+        </div>
+        <div className="encode__item">
+          <p className="encode__outputText">{output || ""}</p>
+        </div>
+      </div>
     </div>
   );
 }
+
 
 export default Encode;
